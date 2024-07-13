fb526e7c4f41130ffa3a56a0d936c837ad0f85ef
Karine Lessa <karinelessa00@gmail.com>
2024-07-13 19:05:10 -0300

    Exercícios S07

---
+++ exercicios/para-sala/exercicio_alunos.sql
@@ -0,0 +1,10 @@
+
+--CREATE TABLE alunos (
+   -- id INTEGER PRIMARY KEY,
+   -- nome TEXT,
+  --  idade INTEGER
+--);
+SELECT * FROM alunos
+INSERT INTO alunos (nome, idade) VALUES ('Daiana', 36);
+INSERT INTO alunos (nome, idade) VALUES ('Angelina', 33);
+INSERT INTO alunos (nome, idade) VALUES ('Raquel', 41);
